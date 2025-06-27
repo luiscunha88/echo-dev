b0ee24aa6413b506955f92f3b83d678a9bab08dc
Luís Cunha <82886735+luiscunha88@users.noreply.github.com>
2025-06-27 07:22:06 +0000

    adiciona página inicial

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home() {
+    return <h1>Marisa! A partir de hoje não compro mais pipocas nem chupas para ninguém!</h1>
+}
+
+export default Home;
+// This is a simple Next.js page component that renders a heading with the text "Marisa! A partir de hoje não compro mais pipocas nem chupas para ninguém!".
+// It can be used as the main page of a Next.js application.
+// To run this code, ensure you have Next.js set up in your project.
+
